7b48792ac00decb9309a8f961f47ee32ce8448df
Derya Uyargil <deryauyargil@hotmail.de>
2016-09-25 22:23:36 +0200

    xml Reader funktinoniert

--- src/aufgabenblatt1aufgabe2/Sensor.java
+++ src/aufgabenblatt1aufgabe2/Sensor.java
@@ -9,13 +9,11 @@ public class Sensor {
 	List<Messung> list = new ArrayList<Messung>();
 	
 	public Sensor(String id){
-		this.id = id;	
+		this.id = id; 
 	}
 	
 	public static void main(String[] args) {
 		Sensor sensor1 = new Sensor("penis");
-		
-		
 		Messung messung1 = new Messung(5.2);
 		System.out.print(messung1.toString());
 		
