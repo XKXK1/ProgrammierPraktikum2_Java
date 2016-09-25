7ca391d979dd7b8bd52c599133e2834fd775b6c4
Derya Uyargil <deryauyargil@hotmail.de>
2016-09-25 15:34:40 +0200

    -

---
+++ src/aufgabenblatt1aufgabe2/Messung.java
@@ -0,0 +1,15 @@
+package aufgabenblatt1aufgabe2;
+
+public class Messung {
+
+	private double wert;
+	private String zeitstempel;
+	
+	public Messung(double wert, String zeitstempel){
+		this.wert = wert;
+		this.zeitstempel = zeitstempel;
+		/**
+		 * 
+		 */
+	}
+}
