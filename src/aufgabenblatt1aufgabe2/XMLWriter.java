b723278e5fa728ec151dac132d7b1698c889c4df
Derya Uyargil <deryauyargil@hotmail.de>
2016-10-02 21:17:50 +0200

    -

--- src/aufgabenblatt1aufgabe2/XMLWriter.java
+++ src/aufgabenblatt1aufgabe2/XMLWriter.java
@@ -7,8 +7,9 @@ import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Node;
 
-
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -23,73 +24,69 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
 public class XMLWriter {
-	
-	  public static void main(String[] args) {
-	        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-	        DocumentBuilder dBuilder;
-	        try {
-	            dBuilder = dbFactory.newDocumentBuilder();
-	            Document doc = dBuilder.newDocument();
-	            //add elements to Document
-	            Element rootElement =
-	                doc.createElementNS("http://www.journaldev.com/employee", "Employees");
-	            //append root element to document
-	            doc.appendChild(rootElement);
-	 
-	            //append first child element to root element
-	            rootElement.appendChild(getSensor(doc, "1", "Pankaj", "29", "1400"));
-	 
-	            //append second child
-	            rootElement.appendChild(getSensor(doc, "2", "Lisa", "35", "20000"));
-	 
-	            //for output to file, console
-	            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-	            Transformer transformer = transformerFactory.newTransformer();
-	            //for pretty print
-	            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-	            DOMSource source = new DOMSource(doc);
-	 
-	            //write to console or file
-	            StreamResult console = new StreamResult(System.out);
-	            StreamResult file = new StreamResult(new File("C:/Users/dry/Desktop/emps.xml"));
-	 
-	            //write data
-	            transformer.transform(source, console);
-	            transformer.transform(source, file);
-	            System.out.println("DONE");
-	 
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
-	    }
-	  
-	   private static Node getSensor(Document doc, String id, String messung, String wert, String zeitstempel) {
-	        Element sensor = doc.createElement("Sensor");
-	 
-	        //set id attribute
-	        sensor.setAttribute("id", id);
-	 
-	        //create messung element
-	        sensor.appendChild(getSensorElements(doc, sensor, "messung", messung));
-	        
-	        
-	        sensor.setAttribute("wert", wert);
-	        
-	        sensor.setAttribute("zeitstempel", zeitstempel);
 
+	public static void main(String[] args) {
+		List<Messung> list = new ArrayList<>();
+		list.add(new Messung(2.5,"1400"));
+		Sensor sensor1 = new Sensor("wohnzimmer", list);
+		
+		
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder;
 
-	 
-	        return sensor;
-	    }
-	 
-	 
-	    //utility method to create text node
-	    private static Node getSensorElements(Document doc, Element element, String name, String value) {
-	        Element node = doc.createElement(name);
-	        node.appendChild(doc.createTextNode(value));
-	        return node;
-	    }
+		try {
+			dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.newDocument();
+			// add elements to Document
+			Element rootElement = doc.createElementNS(sensor1.getID(),"Sensor" );
+			// append root element to document
+			doc.appendChild(rootElement);
+
+			// append first child element to root element
+			 rootElement.appendChild(getSensor(doc, "1", "Pankaj", "29", "1400"));
+
+			// for output to file, console
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			// for pretty print
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			DOMSource source = new DOMSource(doc);
+
+			// write to console or file
+			StreamResult console = new StreamResult(System.out);
+			StreamResult file = new StreamResult(new File("C:/Users/dry/Desktop/emps.xml"));
+
+			// write data
+			transformer.transform(source, console);
+			transformer.transform(source, file);
+			System.out.println("DONE");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static Node getSensor(Document doc, String id, String messung, String wert, String zeitstempel) {
+		Element sensor = doc.createElement("Sensor");
+
+		// set id attribute
+		sensor.setAttribute("id", id);
+
+		// create messung element
+		sensor.appendChild(getSensorElements(doc, sensor, "messung", messung));
+
+		sensor.setAttribute("wert", wert);
+
+		sensor.setAttribute("zeitstempel", zeitstempel);
+
+		return sensor;
+	}
+
+	// utility method to create text node
+	private static Node getSensorElements(Document doc, Element element, String name, String value) {
+		Element node = doc.createElement(name);
+		node.appendChild(doc.createTextNode(value));
+		return node;
+	}
 
 }
-
-
