656c9e76ef0a1330202ec52cd3110958469a15c6
Daniel von Drathen <danielvondrathen@hotmail.de>
2016-10-03 14:33:13 +0200

    Absaetze
    Formatierung

--- src/aufgabenblatt1aufgabe3/ArrayListe.java
+++ src/aufgabenblatt1aufgabe3/ArrayListe.java
@@ -3,23 +3,22 @@ package aufgabenblatt1aufgabe3;
 public class ArrayListe<T> {
   private int anzahlElemente = 0;
   private Object[] elemente = new Object[2];
-  
 
   public void hinzufuegen(T element) {
-    if (getAnzahlElemente() == elemente.length){
-      Object[] duplicate = new Object[elemente.length*2];
-      System.arraycopy( elemente, 0, duplicate, 0, elemente.length );
+    if (getAnzahlElemente() == elemente.length) {
+      Object[] duplicate = new Object[elemente.length * 2];
+      System.arraycopy(elemente, 0, duplicate, 0, elemente.length);
       elemente = (T[]) duplicate;
     }
-      elemente[anzahlElemente] = (Object)element;
-      anzahlElemente++;
+    elemente[anzahlElemente] = (Object) element;
+    anzahlElemente++;
 
   }
 
   public T get(int index) {
-    try{
-    return (T) elemente[index];
-    }catch(IndexOutOfBoundsException e){
+    try {
+      return (T) elemente[index];
+    } catch (IndexOutOfBoundsException e) {
       System.out.println("Es gibt kein Element an dieser Stelle!");
       return null;
     }
@@ -31,10 +30,10 @@ public class ArrayListe<T> {
   }
 
   public void entferneElementAnIndex(int index) {
-    for (int i = index; i< elemente.length-1; i++){
-      elemente[i]=elemente[i+1];
+    for (int i = index; i < elemente.length - 1; i++) {
+      elemente[i] = elemente[i + 1];
     }
-    elemente[elemente.length-1]= null;
+    elemente[elemente.length - 1] = null;
 
     anzahlElemente--;
   }
@@ -46,7 +45,7 @@ public class ArrayListe<T> {
   @Override
   public String toString() {
     String ausgabe = "";
-    for (int i = 0; i<getAnzahlElemente();i++){
+    for (int i = 0; i < getAnzahlElemente(); i++) {
       ausgabe += elemente[i] + "\n";
     }
     return ausgabe;
@@ -55,24 +54,22 @@ public class ArrayListe<T> {
   public T getKleinstesElement() {
     return null;
   }
-  
-  public static void main(String[] args){
+
+  public static void main(String[] args) {
     ArrayListe<String> testListe = new ArrayListe<String>();
     testListe.hinzufuegen("Hans");
     testListe.hinzufuegen("Hipster");
     testListe.hinzufuegen("Chill");
     testListe.hinzufuegen("Peter");
-    
 
-    
     System.out.println(testListe);
-    
+
     testListe.entferneElementAnIndex(2);
-    
+
     System.out.println(testListe);
     System.out.println(testListe.getAnzahlElemente());
-    
+
     testListe.get(4);
-    
+
   }
 }
--- src/aufgabenblatt1aufgabe3/TestArrayListe.java
+++ src/aufgabenblatt1aufgabe3/TestArrayListe.java
@@ -12,29 +12,29 @@ public class TestArrayListe {
     testListe.hinzufuegen("Test1");
     testListe.hinzufuegen("Test2");
     testListe.hinzufuegen("Test3");
-    
+
     int erwartungsAnzahl = 3;
-    
+
     assertEquals(erwartungsAnzahl, testListe.getAnzahlElemente());
   }
-  
+
   @Test
-  public void testEntfernen(){
+  public void testEntfernen() {
     ArrayListe<String> testListe = new ArrayListe<String>();
     testListe.hinzufuegen("Test1");
     testListe.hinzufuegen("Test2");
     testListe.hinzufuegen("Test3");
     testListe.hinzufuegen("Test4");
-    
+
     testListe.entferneElementAnIndex(0);
-    
+
     int erwartungAnzahl = 3;
     String erwartungsString = "Test2";
     String realerString = testListe.get(0);
-    
+
     assertNotEquals(5, testListe.getAnzahlElemente());
     assertEquals(erwartungAnzahl, testListe.getAnzahlElemente());
-    assertTrue(realerString.compareTo(erwartungsString)==0);
+    assertTrue(realerString.compareTo(erwartungsString) == 0);
   }
 
 }
