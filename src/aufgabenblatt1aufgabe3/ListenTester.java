e1987c32cdf6e21d4af8a4e3897e7a55be390ce0
Saintsaw <danielvondrathen@hotmail.de>
2016-10-05 17:42:20 +0200

    ListenTester
    Erstellen der Listen Überprüferklasse

---
+++ src/aufgabenblatt1aufgabe3/ListenTester.java
@@ -0,0 +1,16 @@
+package aufgabenblatt1aufgabe3;
+
+public class ListenTester {
+	
+	public static boolean listenTest(ArrayListe liste){
+		if(liste.get(0)!=null){
+	//	if(liste.get(0).getClass().isInstance(){
+			
+		//}
+			
+		}
+		return false;
+		
+	}
+
+}
