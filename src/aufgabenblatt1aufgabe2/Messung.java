72096c94a3222d3f7bdda45ef7dc89c4d2ad4662
Derya Uyargil <deryauyargil@hotmail.de>
2016-10-06 10:13:11 +0200

    Coding Style

--- src/aufgabenblatt1aufgabe2/Messung.java
+++ src/aufgabenblatt1aufgabe2/Messung.java
@@ -1,20 +1,29 @@
 package aufgabenblatt1aufgabe2;
 
+/**
+ * 
+ * @author Derya Uyargil, Daniel von Drathen
+ * 
+ *         Ein Objekt der KlasseMessung besteht aus "wert"(double) und
+ *         "zeitstempel"(String). Er repraesentiert eine Messung eines
+ *         Sensor-Objektes.
+ *
+ */
 public class Messung {
 
-  private double wert;
-  private String zeitstempel;
+	private double wert;
+	private String zeitstempel;
 
-  public Messung(double wert, String zeitstempel) {
-    this.wert = wert;
-    this.zeitstempel = zeitstempel;
-  }
+	public Messung(double wert, String zeitstempel) {
+		this.wert = wert;
+		this.zeitstempel = zeitstempel;
+	}
 
-  public double getWert() {
-    return wert;
-  }
+	public double getWert() {
+		return wert;
+	}
 
-  public String getZeitstempel() {
-    return zeitstempel;
-  }
+	public String getZeitstempel() {
+		return zeitstempel;
+	}
 }
