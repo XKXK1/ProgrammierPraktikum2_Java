e96059d82f031bf100e8f378667aaad4ad394ad2
Derya Uyargil <deryauyargil@hotmail.de>
2016-10-06 09:58:50 +0200

    Coding style

--- src/aufgabenblatt1aufgabe1/Pruefungsleistung.java
+++ src/aufgabenblatt1aufgabe1/Pruefungsleistung.java
@@ -1,29 +1,32 @@
 package aufgabenblatt1aufgabe1;
+
 /**
  * 
  * @author Derya Uyargil, Daniel von Drathen
  * 
- * Ein Objekt der Klasse Pruefungsleistung besteht aus "note"(int) und "modulname"(String).
- * Er repraesentiert eine Pruefungsleistung eines Studenten.
+ *         Ein Objekt der Klasse Pruefungsleistung besteht aus "note"(int) und
+ *         "modulname"(String). Er repraesentiert eine Pruefungsleistung eines
+ *         Studenten.
  *
  */
 
 public class Pruefungsleistung {
 	private int note;
 	private String modulname;
-	
+
 	/**
 	 * Der Konstruktor erwartet folgende zwei Argumente:
+	 * 
 	 * @param note
 	 * @param modulname
 	 */
-	public Pruefungsleistung(int note, String modulname){
+	public Pruefungsleistung(int note, String modulname) {
 		this.note = note;
 		this.modulname = modulname;
 	}
+
 	/**
-	 * Ueberschreiben der toString Methode
-	 *  Note und Modulname werden ausgegeben.
+	 * Ueberschreiben der toString Methode Note und Modulname werden ausgegeben.
 	 */
 
 	@Override
