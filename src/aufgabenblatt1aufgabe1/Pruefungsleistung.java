10d08d6a9ef66cfa8bfca8b802084319955daf41
Derya Uyargil <deryauyargil@hotmail.de>
2016-10-06 09:55:28 +0200

    Kommentare angepasst.

--- src/aufgabenblatt1aufgabe1/Pruefungsleistung.java
+++ src/aufgabenblatt1aufgabe1/Pruefungsleistung.java
@@ -1,9 +1,22 @@
 package aufgabenblatt1aufgabe1;
+/**
+ * 
+ * @author Derya Uyargil, Daniel von Drathen
+ * 
+ * Die Objekt der Klasse Pruefungsleistung besteht aus "note"(int) und "modulname"(String).
+ * Er repraesentiert eine Pruefungsleistung eines Studenten.
+ *
+ */
 
 public class Pruefungsleistung {
 	private int note;
 	private String modulname;
 	
+	/**
+	 * Der Konstruktor erwartet folgende zwei Argumente:
+	 * @param note
+	 * @param modulname
+	 */
 	public Pruefungsleistung(int note, String modulname){
 		this.note = note;
 		this.modulname = modulname;
