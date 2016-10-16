28d626e7fdf9009d4cd7063ab7f6f2555d8df3a1
Daniel von Drathen <danielvondrathen@hotmail.de>
2016-10-16 16:07:07 +0200

    Lambdas Fertig

--- src/augabenblatt2_1/Rechner.java
+++ src/augabenblatt2_1/Rechner.java
@@ -1,41 +1,33 @@
 package augabenblatt2_1;
 
 import java.util.HashMap;
-import java.util.Map;
 import java.util.function.BinaryOperator;
 
 public class Rechner {
+  HashMap<Operation, BinaryOperator<Double>> hm;
 
-//  Map<Operation, BinaryOperator>aufgabe = new HashMap<Operation, BinaryOperator>();
-//  aufgabe.put(ADDITION, BinaryOperator<String> binaryOpt = );
-	
-	private DoubleDoubleZuDouble addition = (zahl1, zahl2) -> {return zahl1 + zahl2;};
-	private DoubleDoubleZuDouble subtraktion = (zahl1, zahl2) -> {return zahl1 - zahl2;};
-	private DoubleDoubleZuDouble multiplikation = (zahl1, zahl2) -> {return zahl1 * zahl2;};
-	private DoubleDoubleZuDouble division = (zahl1, zahl2) -> {return zahl1 / zahl2;};
-	
-	Map<Operation, BinaryOperator<DoubleDoubleZuDouble>> hm; 
-	
-	public void hashMaptest(){
-		new HashMap<>();
-	    hm.put(Operation.ADDITION, (BinaryOperator<DoubleDoubleZuDouble>) addition);
-	    hm.put(Operation.SUBTRAKTION, (BinaryOperator<DoubleDoubleZuDouble>) subtraktion);
-	    hm.put(Operation.MULTIPLIKATION, (BinaryOperator<DoubleDoubleZuDouble>) multiplikation);
-	    hm.put(Operation.DIVISION, (BinaryOperator<DoubleDoubleZuDouble>) division);    
-    }
-    
-	public double berechne(Operation operator, double zahl1, double zahl2){
-		
-		return 1.0;
-	}
-    
+  public Rechner() {
+    hm = new HashMap<Operation, BinaryOperator<Double>>();
+    hm.put(Operation.ADDITION, (zahl1, zahl2) -> {
+      return zahl1 + zahl2;
+    });
+    hm.put(Operation.SUBTRAKTION, (zahl1, zahl2) -> {
+      return zahl1 - zahl2;
+    });
+    hm.put(Operation.MULTIPLIKATION, (zahl1, zahl2) -> {
+      return zahl1 * zahl2;
+    });
+    hm.put(Operation.DIVISION, (zahl1, zahl2) -> {
+      return zahl1 / zahl2;
+    });
+  }
 
-	
+  public double berechne(Operation operator, double zahl1, double zahl2) {
+    return hm.get(operator).apply(zahl1, zahl2);
+  }
 
-	
-	 
-	
-
-
-
+  public static void main(String[] args) {
+    Rechner rechner = new Rechner();
+    System.out.println(rechner.berechne(Operation.ADDITION, 12, 11));
+  }
 }
--- src/augabenblatt2_1/TestLambda.java
+++ src/augabenblatt2_1/TestLambda.java
@@ -9,33 +9,47 @@ public class TestLambda {
   @Test
   public void testAddition() {
     Rechner rechner = new Rechner();
-    double ergebnis = rechner.berechne(Operation.ADDITION, 12, 11);
-    double erwartetesErgebnis = 23;
-    assertEquals("Fehler: Es wurde 23 erwartet!", erwartetesErgebnis, ergebnis, 0.001);
+    double ergebnis = rechner.berechne(Operation.ADDITION, 12.1, 11.0);
+    double erwartetesErgebnis = 23.1;
+    assertEquals("Fehler: Es wurde 23.1 erwartet!", erwartetesErgebnis,
+        ergebnis, 0.001);
   }
 
   @Test
   public void testSubtraktion() {
     Rechner rechner = new Rechner();
-    double ergebnis = rechner.berechne(Operation.SUBTRAKTION, 12, 11);
-    double erwartetesErgebnis = 1;
-    assertEquals("Fehler: Es wurde 1 erwartet!", erwartetesErgebnis, ergebnis, 0.001);
+    double ergebnis = rechner.berechne(Operation.SUBTRAKTION, 12.1, 11.0);
+    double erwartetesErgebnis = 1.1;
+    assertEquals("Fehler: Es wurde 1,2 erwartet!", erwartetesErgebnis, ergebnis,
+        0.001);
   }
 
   @Test
   public void testDivision() {
     Rechner rechner = new Rechner();
-    double ergebnis = rechner.berechne(Operation.DIVISION, 12, 6);
-    double erwartetesErgebnis = 2;
-    assertEquals("Fehler: Es wurde 2 erwartet!", erwartetesErgebnis, ergebnis, 0.001);
+    double ergebnis = rechner.berechne(Operation.DIVISION, 12.1, 6.0);
+    double erwartetesErgebnis = 2.016;
+    assertEquals("Fehler: Es wurde 2 erwartet!", erwartetesErgebnis, ergebnis,
+        0.1);
   }
 
   @Test
   public void testMultiplikation() {
     Rechner rechner = new Rechner();
-    double ergebnis = rechner.berechne(Operation.MULTIPLIKATION, 12, 11);
-    double erwartetesErgebnis = 132;
-    assertEquals("Fehler: Es wurde 132 erwartet!", erwartetesErgebnis, ergebnis, 0.001);
+    double ergebnis = rechner.berechne(Operation.MULTIPLIKATION, 12.1, 11.0);
+    double erwartetesErgebnis = 133.1;
+    assertEquals("Fehler: Es wurde 133.1 erwartet!", erwartetesErgebnis,
+        ergebnis, 0.001);
   }
 
+  @Test
+  public void testDoubDouble() {
+    DoubleDoubleZuDouble samMulti = (zahl1, zahl2) -> {
+      return zahl1 * zahl2;};
+
+      double berechnet = samMulti.werteAus(5, 3);
+      
+      assertEquals("Falsch", 15.0, berechnet , 0.1);
+
+    }
 }
