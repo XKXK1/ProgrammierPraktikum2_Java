752790d9cc984213356647c60af3252dcecf3612
Derya Uyargil <deryauyargil@hotmail.de>
2016-10-23 18:29:16 +0200

    Flugzeug - landen(fehlt) Flughafen

--- src/aufgabenblatt2_3/Flugzeug.java
+++ src/aufgabenblatt2_3/Flugzeug.java
@@ -1,61 +1,67 @@
 package aufgabenblatt2_3;
 
 public class Flugzeug extends Thread {
-  private Flughafen flughafen;
-  private String id;
-  private int flugdauer;
-  private int startzeit;
-  private int zeitLandeanflug;
-  private Status status;
+	private Flughafen flughafen;
+	private String id;
+	private int flugdauer;
+	private int startzeit;
+	private int zeitLandeanflug;
+	public Status status;
 
-  public Flugzeug(String id, int flugdauer, Flughafen flughafen,
-      int startzeit) {
-    this.id = id;
-    this.flugdauer = flugdauer;
-    this.flughafen = flughafen;
-    this.startzeit = startzeit;
-  }
+	public Flugzeug(String id, int flugdauer, Flughafen flughafen, int startzeit) {
+		this.id = id;
+		this.flugdauer = flugdauer;
+		this.flughafen = flughafen;
+		this.startzeit = startzeit;
+		this.status = Status.IM_FLUG;
+	}
 
-  @Override
-  public void run() {
-    while (!isInterrupted()) {
-      if (flughafen.getZeit() - this.startzeit >= this.flugdauer) {
-        if (flughafen.landebahnFrei) {
-          this.status = Status.IM_LANDEANFLUG;
-          this.zeitLandeanflug = flughafen.getZeit();
-          flughafen.landebahnFrei = false;
+	@Override
+	public void run() {
+		while (!isInterrupted()) {
+			if (flughafen.getZeit() - this.startzeit >= this.flugdauer) {
+				if (flughafen.landebahnFrei) {
+					this.status = Status.IM_LANDEANFLUG;
+					this.zeitLandeanflug = flughafen.getZeit();
+					flughafen.landebahnFrei = false;
 
-        }
-        if (this.status == Status.IM_LANDEANFLUG) {
-          if (flughafen.getZeit() - this.zeitLandeanflug >= 1500) {
-            this.status = status.GELANDET;
-            flughafen.landebahnFrei = true;
-            flughafen.anzahlFlugzeuge--;
-          }
+				}
 
-        }
+			}
+			if (this.status == Status.IM_LANDEANFLUG) {
+				if (flughafen.getZeit() - this.zeitLandeanflug >= 1500) {
+					this.status = status.GELANDET;
+					flughafen.landebahnFrei = true;
+					flughafen.anzahlFlugzeuge--;
+					interrupt();
 
-      }
+				}
 
-    }
+			}
 
-  }
+		}
 
-  public void istGelandet() {
-    if (isGelandet()) {
-      status = Status.GELANDET;
-    }
-  }
+	}
 
-  @Override
-  public String toString() {
-    return "Flugzeug " + id + " (" + status + ", Zeit bis Ziel: " + (flugdauer-(flughafen.getZeit() - this.startzeit));
-  }
+	public void istGelandet() {
+		if (isGelandet()) {
+			status = Status.GELANDET;
+		}
+	}
 
-  public boolean isGelandet() {
+	@Override
+	public String toString() {
+		if(flugdauer - (flughafen.getZeit() - this.startzeit)<=0){
+			return "\nFlugzeug " + id + " (" + status + ", Zeit bis Ziel: 0 )";
+		}
+		return "\nFlugzeug " + id + " (" + status + ", Zeit bis Ziel: "
+				+ (flugdauer - (flughafen.getZeit() - this.startzeit)) + ")";
+	}
 
-    istGelandet();
-    return true;
-  }
+	public boolean isGelandet() {
+
+		istGelandet();
+		return true;
+	}
 
 }
