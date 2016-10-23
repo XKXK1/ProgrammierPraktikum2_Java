8b0dc56db7eccc6d43135fc5c12930ecd2d19cec
Daniel von Drathen <danielvondrathen@hotmail.de>
2016-10-23 21:11:23 +0200

    Synchronize
    Spielereien

--- src/aufgabenblatt2_3/Flugzeug.java
+++ src/aufgabenblatt2_3/Flugzeug.java
@@ -5,7 +5,6 @@ public class Flugzeug extends Thread {
   private String id;
   private int flugdauer;
   private int startzeit;
-  private int zeitLandeanflug;
   public Status status;
 
   public Flugzeug(String id, int flugdauer, Flughafen flughafen,
@@ -20,22 +19,52 @@ public class Flugzeug extends Thread {
   @Override
   public void run() {
     while (!isInterrupted()) {
-      if (flughafen.getZeit() - this.startzeit >= this.flugdauer) {
-        if (flughafen.landebahnFrei) {
-          landeanflug();
-        }
+      while(!ankunft()){
+          try {
+            Thread.sleep(250);
+          } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+          }
+      }if (flughafen.landebahnFrei){
+        landeanflug();
       }
       istGelandet();
+      }
     }
+
+  private boolean ankunft() {
+    if (flughafen.getZeit() - this.startzeit >= this.flugdauer) {
+      return true;
+    }
+    return false;
   }
 
   public void istGelandet() {
     if (this.status == Status.IM_LANDEANFLUG) {
-      if (flughafen.getZeit() - this.zeitLandeanflug >= 1500) {
-        this.status = Status.GELANDET;
-        flughafen.landebahnFrei = true;
-        flughafen.anzahlFlugzeuge--;
-        interrupt();
+
+      this.status = Status.GELANDET;
+      flughafen.landebahnFrei = true;
+      flughafen.anzahlFlugzeuge--;
+      interrupt();
+    }
+  }
+
+  public boolean isGelandet() {
+    if (status == Status.GELANDET) {
+      return true;
+    }
+    return false;
+  }
+
+  public synchronized void landeanflug() {
+    if (flughafen.landebahnFrei) {
+      status = Status.IM_LANDEANFLUG;
+      flughafen.landebahnFrei = false;
+      try {
+        Thread.sleep(1500);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
       }
     }
   }
@@ -47,23 +76,6 @@ public class Flugzeug extends Thread {
     }
     return "\nFlugzeug " + id + " (" + status + ", Zeit bis Ziel: "
         + (flugdauer - (flughafen.getZeit() - this.startzeit)) + ")";
-  }
-
-  public boolean isGelandet() {
-    if (status == Status.GELANDET) {
-      return true;
-    }
-    return false;
-  }
-
-  public synchronized void landeanflug() {
-    status = Status.IM_LANDEANFLUG;
-    this.zeitLandeanflug = flughafen.getZeit();
-    flughafen.landebahnFrei = false;
-  }
-  
-  public String getID(){
-    return id;
   }
 
 }
