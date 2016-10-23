af8f08f5cc5b40b0ca0dcba4b8356792bf168274
Daniel von Drathen <danielvondrathen@hotmail.de>
2016-10-23 19:59:47 +0200

    Syncronization and random names
    Angefangen eine Methode zu Syncronisieren, Run methode aufgeräumt und
    einen Random Namensgenerator geschrieben für die Flugzeuge

--- src/aufgabenblatt2_3/Flughafen.java
+++ src/aufgabenblatt2_3/Flughafen.java
@@ -4,78 +4,81 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Flughafen extends Thread {
-	Flugzeug flugzeug;
-	public int anzahlFlugzeuge = 0;
-	private final int MAX_FLUGZEUGE = 10;
-	int flugzeugNummer = 1000;
-	private int zeit;
-	public boolean landebahnFrei = true;
+  Flugzeug flugzeug;
+  public int anzahlFlugzeuge = 0;
+  private final int MAX_FLUGZEUGE = 10;
+  int flugzeugNummer = 1000;
+  private int zeit;
+  public boolean landebahnFrei = true;
+  String[] fluggesellschaft = {"Lufthansa", "Air Berlin", "Air France",
+      "EuroWings", "Turkish Airlines"};
 
-	List<Flugzeug> flugzeuge = new ArrayList<Flugzeug>();
+  List<Flugzeug> flugzeuge = new ArrayList<Flugzeug>();
 
-	public Flughafen(int anzahlFlugzeuge) {
-		this.anzahlFlugzeuge = anzahlFlugzeuge;
-	}
+  public Flughafen(int anzahlFlugzeuge) {
+    this.anzahlFlugzeuge = anzahlFlugzeuge;
+  }
 
-	public void landen(Flugzeug flugzeug) {
-	}
+  public void landen(Flugzeug flugzeug) {
+  }
 
-	@Override
-	public void run() {
+  @Override
+  public void run() {
 
-		while (!isInterrupted()) {
-			try {
-				if (anzahlFlugzeuge <= MAX_FLUGZEUGE) {
-					flugzeuge.add(erzeugeFlugzeug(this, zeit));
-				}
-				Thread.sleep(500);
-				zeit += 500;
-			} catch (InterruptedException e) {
-				System.err.println("Thread wurde durch Interrupt angesprochen");
-			}
-			System.out.format(this.toString());
-			
-			for (int i = 0; i <flugzeuge.size(); i++){
-					if(flugzeuge.get(i).status == Status.GELANDET){
-						flugzeuge.remove(i);
-					}
-			}
-		}
-		System.err.println("Thead beendet");
-	}
+    while (!isInterrupted()) {
+      try {
+        if (anzahlFlugzeuge <= MAX_FLUGZEUGE) {
+          flugzeuge.add(erzeugeFlugzeug(this, zeit));
+        }
+        Thread.sleep(500);
+        zeit += 500;
+      } catch (InterruptedException e) {
+        System.err.println("Thread wurde durch Interrupt angesprochen");
+      }
+      System.out.format(this.toString());
 
-	private Flugzeug erzeugeFlugzeug(Flughafen flughafen, int startzeit) {
-		flugzeugNummer++;
-		anzahlFlugzeuge++;
-		int flugdauer = (int) ((Math.random() + 1) * 5000);
-		String flugzeugName = String.valueOf(flugzeugNummer);
-
-		Flugzeug flugzeug = new Flugzeug(flugzeugName, flugdauer, flughafen, startzeit);
-		flugzeug.start();
-		System.out.println("->Neues Flugzeug wurde erzeugt: " + flugzeug);
-		return flugzeug;
-	}
-
-	@Override
-  public String toString() {
-    String ausgabe = "Zeit: "+zeit;
-    try{
-    	for (int i = 0; i < flugzeuge.size(); i++) {
-            ausgabe += flugzeuge.get(i).toString();
-    	}
-    } catch (IndexOutOfBoundsException e){
-    System.err.println("lol");
+      for (int i = 0; i < flugzeuge.size(); i++) {
+        if (flugzeuge.get(i).isGelandet()) {
+          flugzeuge.remove(i);
+        }
+      }
     }
-    
+    System.err.println("Thead beendet");
+  }
+
+  private Flugzeug erzeugeFlugzeug(Flughafen flughafen, int startzeit) {
+    String flugzeugName = fluggesellschaft[(int)(Math.random()*fluggesellschaft.length)] + " " + (int) (Math.random()*9000);
+    flugzeugNummer++;
+    anzahlFlugzeuge++;
+    int flugdauer = (int) ((Math.random() + 1) * 5000);
+
+    Flugzeug flugzeug = new Flugzeug(flugzeugName, flugdauer, flughafen,
+        startzeit);
+    flugzeug.start();
+    System.out.println("->Neues Flugzeug wurde erzeugt: " + flugzeug);
+    return flugzeug;
+  }
+
+  @Override
+  public String toString() {
+    String ausgabe = "Zeit: " + zeit;
+    try {
+      for (int i = 0; i < flugzeuge.size(); i++) {
+        ausgabe += flugzeuge.get(i).toString();
+      }
+    } catch (IndexOutOfBoundsException e) {
+      System.err.println("Index vom Flugzeugarray ist out of Bounds");
+    }
+
     return ausgabe + "\n \n";
   }
 
-	public static void main(String[] args) {
-		Flughafen flughafenHamburg = new Flughafen(1);
-		flughafenHamburg.start();
-	}
+  public static void main(String[] args) {
+    Flughafen flughafenHamburg = new Flughafen(1);
+    flughafenHamburg.start();
+  }
 
-	public int getZeit() {
-		return zeit;
-	}
+  public int getZeit() {
+    return zeit;
+  }
 }
--- src/aufgabenblatt2_3/Flugzeug.java
+++ src/aufgabenblatt2_3/Flugzeug.java
@@ -1,67 +1,69 @@
 package aufgabenblatt2_3;
 
 public class Flugzeug extends Thread {
-	private Flughafen flughafen;
-	private String id;
-	private int flugdauer;
-	private int startzeit;
-	private int zeitLandeanflug;
-	public Status status;
+  private Flughafen flughafen;
+  private String id;
+  private int flugdauer;
+  private int startzeit;
+  private int zeitLandeanflug;
+  public Status status;
 
-	public Flugzeug(String id, int flugdauer, Flughafen flughafen, int startzeit) {
-		this.id = id;
-		this.flugdauer = flugdauer;
-		this.flughafen = flughafen;
-		this.startzeit = startzeit;
-		this.status = Status.IM_FLUG;
-	}
+  public Flugzeug(String id, int flugdauer, Flughafen flughafen,
+      int startzeit) {
+    this.id = id;
+    this.flugdauer = flugdauer;
+    this.flughafen = flughafen;
+    this.startzeit = startzeit;
+    this.status = Status.IM_FLUG;
+  }
 
-	@Override
-	public void run() {
-		while (!isInterrupted()) {
-			if (flughafen.getZeit() - this.startzeit >= this.flugdauer) {
-				if (flughafen.landebahnFrei) {
-					this.status = Status.IM_LANDEANFLUG;
-					this.zeitLandeanflug = flughafen.getZeit();
-					flughafen.landebahnFrei = false;
+  @Override
+  public void run() {
+    while (!isInterrupted()) {
+      if (flughafen.getZeit() - this.startzeit >= this.flugdauer) {
+        if (flughafen.landebahnFrei) {
+          landeanflug();
+        }
+      }
+      istGelandet();
+    }
+  }
 
-				}
+  public void istGelandet() {
+    if (this.status == Status.IM_LANDEANFLUG) {
+      if (flughafen.getZeit() - this.zeitLandeanflug >= 1500) {
+        this.status = Status.GELANDET;
+        flughafen.landebahnFrei = true;
+        flughafen.anzahlFlugzeuge--;
+        interrupt();
+      }
+    }
+  }
 
-			}
-			if (this.status == Status.IM_LANDEANFLUG) {
-				if (flughafen.getZeit() - this.zeitLandeanflug >= 1500) {
-					this.status = status.GELANDET;
-					flughafen.landebahnFrei = true;
-					flughafen.anzahlFlugzeuge--;
-					interrupt();
+  @Override
+  public String toString() {
+    if (flugdauer - (flughafen.getZeit() - this.startzeit) <= 0) {
+      return "\nFlugzeug " + id + " (" + status + ", Zeit bis Ziel: 0 )";
+    }
+    return "\nFlugzeug " + id + " (" + status + ", Zeit bis Ziel: "
+        + (flugdauer - (flughafen.getZeit() - this.startzeit)) + ")";
+  }
 
-				}
+  public boolean isGelandet() {
+    if (status == Status.GELANDET) {
+      return true;
+    }
+    return false;
+  }
 
-			}
-
-		}
-
-	}
-
-	public void istGelandet() {
-		if (isGelandet()) {
-			status = Status.GELANDET;
-		}
-	}
-
-	@Override
-	public String toString() {
-		if(flugdauer - (flughafen.getZeit() - this.startzeit)<=0){
-			return "\nFlugzeug " + id + " (" + status + ", Zeit bis Ziel: 0 )";
-		}
-		return "\nFlugzeug " + id + " (" + status + ", Zeit bis Ziel: "
-				+ (flugdauer - (flughafen.getZeit() - this.startzeit)) + ")";
-	}
-
-	public boolean isGelandet() {
-
-		istGelandet();
-		return true;
-	}
+  public synchronized void landeanflug() {
+    status = Status.IM_LANDEANFLUG;
+    this.zeitLandeanflug = flughafen.getZeit();
+    flughafen.landebahnFrei = false;
+  }
+  
+  public String getID(){
+    return id;
+  }
 
 }
