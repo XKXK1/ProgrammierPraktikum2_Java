4f5146ffddbe32d2ba996e0d25be9cbe1d07a3f5
pewpe <pewpe@LAPTOP-92BEMKKK>
2016-10-24 09:58:25 +0200

    Kommentare hinzugefuegt

--- src/aufgabenblatt2_1/DoubleDoubleZuDouble.java
+++ src/aufgabenblatt2_1/DoubleDoubleZuDouble.java
@@ -1,6 +1,11 @@
 package aufgabenblatt2_1;
 
+/**
+ * Dieses Funktionale Interface besitzt die Methode werteAus(double, double)
+ * erwartet 2 Argumente des Typs dobuble.
+ *
+ */
 @FunctionalInterface
 public interface DoubleDoubleZuDouble {
-  double werteAus(double dbl1, double dbl2);
+	double werteAus(double dbl1, double dbl2);
 }
--- src/aufgabenblatt2_1/Operation.java
+++ src/aufgabenblatt2_1/Operation.java
@@ -1,5 +1,8 @@
 package aufgabenblatt2_1;
-
+/**
+ * Das Enum repraesentiert die 4 Rechenoperationen unseres Rechners.
+ *
+ */
 public enum Operation {
   ADDITION, SUBTRAKTION, DIVISION, MULTIPLIKATION
 }
